ea6a4e6a49cb29541b4065ccb138b350d061db0f
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-05 17:15:34 +0100

    Create basic wrapper for the Habit SQLite database
    Currently only implements the basic functionalities of getting a connection and seeding data

---
+++ HabitLogger/Data/HabitLoggerDatabase.cs
@@ -0,0 +1,87 @@
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace HabitLogger;
+
+public class HabitLoggerDatabase
+{
+    public static SqliteConnection GetConnection()
+    {
+        var connectionString = "Data Source=habits.db";
+        var connection = new SqliteConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return connection;
+    }
+
+    public static void CreateDatabase()
+    {
+        var connection = GetConnection();
+
+        try
+        {
+            var createTableCommand = connection.CreateCommand();
+            createTableCommand.CommandText =
+                """
+                CREATE TABLE IF NOT EXISTS Habits (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    HabitName TEXT NOT NULL,
+                    Unit TEXT NOT NULL
+                );
+
+                CREATE TABLE IF NOT EXISTS HabitLogs (
+                    LogID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    HabitID INTEGER,
+                    Quantity INTEGER NOT NULL,
+                    Date TEXT NOT NULL,
+                    FOREIGN KEY (HabitID) REFERENCES Habits(ID)
+                );
+
+                """;
+            createTableCommand.ExecuteNonQuery();
+
+
+            var insertSeedDataCommand = connection.CreateCommand();
+            insertSeedDataCommand.CommandText =
+                """
+                -- Seed data for the Habits
+                INSERT INTO Habits (HabitName, Unit) VALUES ('Drinking Water', 'glasses');
+                INSERT INTO Habits (HabitName, Unit) VALUES ('Reading', 'pages');
+
+                -- Seed data for "Drinking Water"
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 8, '2024-01-29');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 7, '2024-01-30');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 9, '2024-01-31');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 8, '2024-02-01');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 7, '2024-02-02');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 8, '2024-02-03');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (1, 9, '2024-02-04');
+
+                -- Seed data for "Reading"
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 30, '2024-01-29');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 25, '2024-01-30');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 35, '2024-01-31');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 40, '2024-02-01');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 20, '2024-02-02');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 45, '2024-02-03');
+                INSERT INTO HabitLogs (HabitID, Quantity, Date) VALUES (2, 50, '2024-02-04');
+
+                """;
+            insertSeedDataCommand.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HabitLogger;
+
+if (!File.Exists("habits.db"))
+{
+    HabitLoggerDatabase.CreateDatabase();
+}
+
+var endApp = false;
+
+while (!endApp)
+{
+    int.TryParse(Console.ReadLine(), out var userChoice);
+
+    switch (userChoice)
+    {
+        case 6:
+            endApp = true;
+            break;
+    }
+}
