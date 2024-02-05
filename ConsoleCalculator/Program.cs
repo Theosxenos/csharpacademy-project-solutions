24ab5bcc44d3077061c846f2778e94c378b482f1
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-05 14:23:45 +0100

    Init calculator app

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,52 @@
+﻿// Declare variables and then initialize to zero.
+float num1 = 0; float num2 = 0;
+
+// Display title as the C# console calculator app.
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+do
+{
+    // Ask the user to type the first number.
+    Console.WriteLine("Type a number, and then press Enter");
+} while (!float.TryParse(Console.ReadLine(), out num1));
+
+do
+{
+    // Ask the user to type the second number.
+    Console.WriteLine("Type another number, and then press Enter");
+} while (!float.TryParse(Console.ReadLine(), out num2));
+
+// Ask the user to choose an option.
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+
+// Use a switch statement to do the math.
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        // Ask the user to enter a non-zero divisor until they do so.
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+// Wait for the user to respond before closing.
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
