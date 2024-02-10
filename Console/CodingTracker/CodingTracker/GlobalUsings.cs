43b67a8613786453b45bf023ca3cbc079e0fbcef
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-10 09:50:32 +0100

    Update GlobalUsings.cs

--- Console/CodingTracker/CodingTracker/GlobalUsings.cs
+++ Console/CodingTracker/CodingTracker/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using CodingTracker.Data;
 global using Spectre.Console;
 global using System.Globalization;
 global using CodingTracker.Repositories;
+global using CodingTracker.Exceptions;
+global using Microsoft.Data.Sqlite;
