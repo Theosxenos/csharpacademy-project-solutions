5cedfdd4639eb600bebd7d904bb8f1b98cc19167
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-12 21:10:06 +0100

    Update GlobalUsings.cs

--- Assessments/BreweryAPI/BreweryAPI/GlobalUsings.cs
+++ Assessments/BreweryAPI/BreweryAPI/GlobalUsings.cs
@@ -1,3 +1,6 @@
 global using Microsoft.AspNetCore.Mvc;
 global using BreweryAPI.Models;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using BreweryAPI.DTOs.Responses;
+global using Microsoft.EntityFrameworkCore;
+global using BreweryAPI.Data;
