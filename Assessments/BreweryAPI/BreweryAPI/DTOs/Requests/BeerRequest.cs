18e9a02d33bcbef7c429065291c30215bee73bc6
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-13 12:02:10 +0100

    Update BeerRequest.cs
    Remove unnecessary data validations

--- Assessments/BreweryAPI/BreweryAPI/DTOs/Requests/BeerRequest.cs
+++ Assessments/BreweryAPI/BreweryAPI/DTOs/Requests/BeerRequest.cs
@@ -4,8 +4,6 @@ public class BeerRequest
 {
     public int BreweryId { get; set; }
     public string Name { get; set; }
-    [Column(TypeName = "decimal(18,2)")]
     public decimal WholesalePrice { get; set; }
-    [Column(TypeName = "decimal(18,2)")]
     public decimal RetailPrice { get; set; }
 }
