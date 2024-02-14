022f346fd7025abd6972dc622bdaa90d962724f5
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-14 14:33:42 +0100

    Add BreweryId  to Order model

--- Assessments/BreweryAPI/BreweryAPI/Models/Order.cs
+++ Assessments/BreweryAPI/BreweryAPI/Models/Order.cs
@@ -5,6 +5,8 @@ public class Order
     public int Id { get; set; }
     public int WholesalerId { get; set; }
     public Wholesaler Wholesaler { get; set; }
+    public int BreweryId { get; set; }
+    public Brewery Brewery { get; set; }
     [Column(TypeName = "decimal(18,2)")]
     public decimal TotalPrice { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
