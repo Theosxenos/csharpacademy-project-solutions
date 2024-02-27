03307f010ff4c13378a706be4274ee266939c911
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-27 11:12:30 +0100

    Fix access modifier in Quote model

--- Assessments/BreweryAPI/BreweryAPI/Models/Quote.cs
+++ Assessments/BreweryAPI/BreweryAPI/Models/Quote.cs
@@ -6,7 +6,7 @@ public class Quote
     public int WholesalerId { get; set; }
     public Wholesaler Wholesaler { get; set; }
     public int Discount { get; set; }
-    private List<QuoteDetail> Quotes { get; set; } = [];
+    public List<QuoteDetail> Quotes { get; set; } = [];
     [Column(TypeName = "decimal(18,2)")]
     public decimal TotalPrice { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
