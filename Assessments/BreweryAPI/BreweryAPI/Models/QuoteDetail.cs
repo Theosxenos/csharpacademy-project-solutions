f8cab90eb6cec0174f0d5d3f702349d7897cf473
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-28 10:50:18 +0100

    Fix infinite recursive call in quote detail

--- Assessments/BreweryAPI/BreweryAPI/Models/QuoteDetail.cs
+++ Assessments/BreweryAPI/BreweryAPI/Models/QuoteDetail.cs
@@ -6,7 +6,6 @@ public class QuoteDetail
     public int BeerId { get; set; }
     public Beer Beer { get; set; }
     public int QuoteId { get; set; }
-    public Quote Quote { get; set; }
     public int Amount { get; set; }
     public float Discount { get; set; }
     [Column(TypeName = "decimal(18,2)")]
