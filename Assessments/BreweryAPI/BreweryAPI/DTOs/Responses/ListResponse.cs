3b565ed745b7fea25365605bcdcf8d3aa5f713c8
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-28 20:59:11 +0100

    Update listresponse DTO
    Comments things for maybe later use

--- Assessments/BreweryAPI/BreweryAPI/DTOs/Responses/ListResponse.cs
+++ Assessments/BreweryAPI/BreweryAPI/DTOs/Responses/ListResponse.cs
@@ -3,11 +3,10 @@ namespace BreweryAPI.DTOs.Responses;
 public class ListResponse<T>
 {
     public IEnumerable<T> Data { get; set; }
-    public int TotalCount { get; set; }
-    public int PageNumber { get; set; }
-    public int PageSize { get; set; }
-    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
-    public bool HasPreviousPage => PageNumber > 1;
-    public bool HasNextPage => PageNumber < TotalPages;
-
+    // public int TotalCount { get; set; }
+    // public int PageNumber { get; set; }
+    // public int PageSize { get; set; }
+    // public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    // public bool HasPreviousPage => PageNumber > 1;
+    // public bool HasNextPage => PageNumber < TotalPages;
 }
