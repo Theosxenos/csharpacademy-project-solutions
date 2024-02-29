290c29bb1a112e55c0e1d910e4358087841520d5
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-02-29 12:35:56 +0100

    Order the sessions by date in the create session view

--- Console/CodingTracker/CodingTracker/Views/CreateSessionView.cs
+++ Console/CodingTracker/CodingTracker/Views/CreateSessionView.cs
@@ -4,12 +4,14 @@ public class CreateSessionView: BaseView
 {
     public Session Prompt(List<Session> sessions)
     {
+        var orderedSessions = sessions.OrderBy(s => s.Day);
+        
         return AnsiConsole.Prompt(
             new SelectionPrompt<Session>()
                 .Title("Select a [green]session[/]")
                 .PageSize(10)
                 .MoreChoicesText("[grey](Move up and down to reveal more sessions)[/]")
-                .AddChoices(sessions)
+                .AddChoices(orderedSessions)
         );
     }
 }
