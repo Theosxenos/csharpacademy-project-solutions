bad10254cde29969e73726d8fc170fbc8bcb0e2b
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-02 11:15:46 +0100

    Adjust SQL Column types of the models

--- Console/Flashcards/Models/Flashcard.cs
+++ Console/Flashcards/Models/Flashcard.cs
@@ -6,7 +6,10 @@ public class Flashcard
     [ForeignKey("Stack")]
     public int StackId { get; set; }
     public Stack Stack { get; set; }
+    [Column(TypeName = "nvarchar(50)")]
     public string Title { get; set; }
+    [Column(TypeName = "nvarchar(250)")]
     public string Question { get; set; }
+    [Column(TypeName = "nvarchar(250)")]
     public string Answer { get; set; }
 }
