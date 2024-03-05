5e12465a1758d52ae52d9fe1f081da002198f450
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-05 20:18:55 +0100

    Call clear before showing menu

--- Console/Flashcards/Views/BaseView.cs
+++ Console/Flashcards/Views/BaseView.cs
@@ -45,6 +45,7 @@ public class BaseView
     
     public T ShowMenu<T>(IEnumerable<T> menuOptions, string title = "Select a menu option:", int pageSize = 10) where T : notnull
     {
+        AnsiConsole.Clear();
         return AnsiConsole.Prompt(new SelectionPrompt<T>()
             .Title(title)
             .PageSize(pageSize)
