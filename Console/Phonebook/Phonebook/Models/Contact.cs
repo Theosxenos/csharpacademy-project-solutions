2aa644eb98ba06129017fe286734ecffcd3596ea
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-06 20:36:24 +0100

    Finished

--- Console/Phonebook/Phonebook/Models/Contact.cs
+++ Console/Phonebook/Phonebook/Models/Contact.cs
@@ -6,4 +6,9 @@ public class Contact
     public string Name { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 }
--- Console/Phonebook/Phonebook/Program.cs
+++ Console/Phonebook/Phonebook/Program.cs
@@ -1,15 +1,35 @@
-﻿using Phonebook.Data;
+﻿using Phonebook.Controllers;
+using Phonebook.Data;
+using Phonebook.Views;
 using Spectre.Console;
 
+var controller = new PhoneBookController();
+var view = new BaseView();
+var runApp = true;
+var mainMenu = new Dictionary<string, Action>
+{
+    ["Add Contact"] = controller.AddContact,
+    ["Update Contact"] = controller.UpdateContact,
+    ["List Contacts"] = controller.ListContacts,
+    ["Delete Contact"] = controller.DeleteContact,
+    ["Exit"] = () => runApp = false
+};
+    
 InitDatabase();
 
 AnsiConsole.Write(new FigletText("Phone Book").Color(Color.Yellow2));
+while (runApp)
+{
+    var choice = view.ShowMenu(mainMenu.Keys.ToArray());
+    mainMenu[choice]();
+}
+
 
 return ;
 
 static void InitDatabase()
 {
     using PhoneBookContext db = new();
-    db.Database.EnsureDeleted();
+    // db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
 }
