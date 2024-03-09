b1f878fee305377424cbcd6952af4a624823413a
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-09 13:51:06 +0100

    Fix showing the name of team that the score are for

--- Console/SportsResultsNotifier/Controllers/MainController.cs
+++ Console/SportsResultsNotifier/Controllers/MainController.cs
@@ -22,8 +22,8 @@ public class MainController(MailService mailService, ScraperService scraperServi
             var teamBName = match.TeamB.IsWinner ? $"**{match.TeamB.Name}**" : match.TeamB.Name;
             body.Append($"# {teamAName} vs {teamBName}{Environment.NewLine}");
             
-            var teamAScore =$"{string.Join("\t", match.TeamA.Score)}\t{match.TeamA.TotalScore}{Environment.NewLine}";
-            var teamBScore = $"{string.Join("\t", match.TeamB.Score)}\t{match.TeamB.TotalScore}{Environment.NewLine}";
+            var teamAScore =$"{teamAName}\t{string.Join("\t", match.TeamA.Score)}\t{match.TeamA.TotalScore}{Environment.NewLine}";
+            var teamBScore = $"{teamBName}\t{string.Join("\t", match.TeamB.Score)}\t{match.TeamB.TotalScore}{Environment.NewLine}";
             body.Append(teamAScore);
             body.Append(teamBScore);
         }
