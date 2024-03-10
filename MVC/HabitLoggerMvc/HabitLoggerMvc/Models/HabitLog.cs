7463d73c8e5f13dc386ad26960ffbc0b40fec55d
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-10 16:30:29 +0100

    Implement viewing logs

--- MVC/HabitLoggerMvc/HabitLoggerMvc/Models/HabitLog.cs
+++ MVC/HabitLoggerMvc/HabitLoggerMvc/Models/HabitLog.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HabitLoggerMvc.Models;
 
 public class HabitLog
 {
     public int Id { get; set; }
     public int HabitId { get; set; }
-    public DateOnly Date { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
     public int Quantity { get; set; }
 }
--- MVC/HabitLoggerMvc/HabitLoggerMvc/Pages/DetailHabit.cshtml.cs
+++ MVC/HabitLoggerMvc/HabitLoggerMvc/Pages/DetailHabit.cshtml.cs
@@ -1,11 +1,27 @@
+using HabitLoggerMvc.Models;
+using HabitLoggerMvc.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HabitLoggerMvc.Pages;
 
-public class DetailHabit : PageModel
+public class DetailHabit(IRepository<Habit> habitRepository, IRepository<HabitUnit> habitUnitRepository, IHabitLogRepository habitLog) : PageModel
 {
-    public void OnGet()
+    [BindProperty]
+    public Habit HabitModel { get; set; }
+    [BindProperty]
+    public HabitUnit HabitUnit { get; set; }
+    [BindProperty]
+    public List<HabitLog> HabitLogs { get; set; }
+    
+    public async Task<IActionResult> OnGetAsync(int id)
     {
+        HabitModel = await habitRepository.GetByIdAsync(id);
+        HabitLogs = (await habitLog.GetByHabitId(id)).ToList();
+        HabitUnit = await habitUnitRepository.GetByIdAsync(HabitModel.HabitUnitId);
         
+        return Page();
     }
+    
+    
 }
