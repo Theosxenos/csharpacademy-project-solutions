9c1b07c36d5615c5a93e1e4f5dc25f990fd0b483
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-10 17:14:59 +0100

    Implement deleting a habit

---
+++ MVC/HabitLoggerMvc/HabitLoggerMvc/Pages/DeleteHabit.cshtml.cs
@@ -0,0 +1,31 @@
+using HabitLoggerMvc.Models;
+using HabitLoggerMvc.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HabitLoggerMvc.Pages;
+
+public class DeleteHabit(IRepository<Habit> habitRepository) : PageModel
+{
+    [BindProperty] public Habit HabitModel { get; set; } = default!;
+    
+    public async Task<IActionResult> OnGetAsync(int id)
+    {
+        if (id > 0)
+        {
+            HabitModel = await habitRepository.GetByIdAsync(id);
+        }
+        else
+        {
+            return NotFound();
+        }
+        
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int id)
+    {
+        await habitRepository.DeleteAsync(id);
+        return RedirectToPage("./Index");
+    }
+}
