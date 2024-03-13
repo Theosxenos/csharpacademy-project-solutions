58835d6fb0ada758262b6836e6cfa3db7ba2aa30
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-13 14:42:17 +0100

    Add seeddata

--- MVC/MvcMovie/MvcMovie/Data/MvcMovieContext.cs
+++ MVC/MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -6,5 +6,43 @@ namespace MvcMovie.Data
     public class MvcMovieContext(DbContextOptions<MvcMovieContext> options) : DbContext(options)
     {
         public DbSet<Movie> Movie { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id =1,
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Id=2,
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Id=3,
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Id =4,
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+                }
+            );
+        }
     }
 }
