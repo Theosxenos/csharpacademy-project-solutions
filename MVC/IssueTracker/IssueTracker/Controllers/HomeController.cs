01ce2a030e8351d46e41e066de87a6ffd4314526
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-16 11:01:02 +0100

    Implement search function

--- MVC/IssueTracker/IssueTracker/Controllers/HomeController.cs
+++ MVC/IssueTracker/IssueTracker/Controllers/HomeController.cs
@@ -2,15 +2,25 @@ using System.Diagnostics;
 using IssueTracker.Data;
 using Microsoft.AspNetCore.Mvc;
 using IssueTracker.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace IssueTracker.Controllers;
 
 public class HomeController(IssueTrackerContext context) : Controller
 {
-    public IActionResult Index()
-    {
-        return View(context.Issues.Include(i => i.Project).Include(i => i.Assignee));
+    public async Task<IActionResult> Index(string? searchTerm, IssueStatus status)
+    {   
+        var issues = context.Issues.Include(i => i.Project).Include(i => i.Assignee)
+            .Where(i => string.IsNullOrEmpty(searchTerm) || i.Title.Contains(searchTerm) && i.Status == status);
+        var vm = new IssueSearchViewModel
+        {
+            Issues = await issues.ToListAsync(),
+            SearchTerm = searchTerm,
+            Statuses = new SelectList(Enum.GetValues<IssueStatus>())
+        };
+        
+        return View(vm);
     }
 
     public IActionResult Privacy()
