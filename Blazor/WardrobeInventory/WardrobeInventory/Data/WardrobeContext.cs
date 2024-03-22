da9e03296862bd1c0d720ff938021fda5177b23c
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-22 10:45:39 +0100

    Attempt install sqlite support

--- Blazor/WardrobeInventory/WardrobeInventory/Data/WardrobeContext.cs
+++ Blazor/WardrobeInventory/WardrobeInventory/Data/WardrobeContext.cs
@@ -6,46 +6,4 @@ namespace WardrobeInventory.Data;
 public class WardrobeContext(DbContextOptions options, HttpClient httpClient) : DbContext(options)
 {
     public DbSet<WardrobeItem> WardrobeItems { get; set; } = default!;
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<WardrobeItem>().HasData(
-            new WardrobeItem
-            {
-                Id = 1,
-                Name = "My favourite shirt",
-                Category = Category.Shirts,
-                Brand = "Grandma",
-                Size = Size.M,
-                ImageData = ConvertImageToBytes("item1.png").Result
-            },
-            new WardrobeItem
-            {
-                Id = 2,
-                Name = "My favourite coat",
-                Category = Category.Dress,
-                Brand = "Prada",
-                Size = Size.L,
-                ImageData = ConvertImageToBytes("item2.png").Result
-            }
-        );
-    }
-
-    // private byte[] ConvertImageToBytes(string imageName)
-    // {
-    //     var imagePath = $"./wwwroot/sample-images/{imageName}";
-    //
-    //
-    //
-    //     return File.ReadAllBytes(imagePath);
-    // }
-    
-    private async Task<byte[]> ConvertImageToBytes(string imageName)
-    {
-        var imagePath = Path.Combine("wwwroot", "sample-images", imageName);
-    
-        // var image = 
-        
-        return await httpClient.GetByteArrayAsync($"sample-images/{imageName}");
-    }
 }
--- Blazor/WardrobeInventory/WardrobeInventory/Program.cs
+++ Blazor/WardrobeInventory/WardrobeInventory/Program.cs
@@ -4,15 +4,20 @@ using Microsoft.EntityFrameworkCore;
 using WardrobeInventory;
 using WardrobeInventory.Data;
 using WardrobeInventory.Services;
+using SqliteWasmHelper;
+using WardrobeInventory.Model;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Configuration.AddUserSecrets<Program>();
 
-builder.Services.AddDbContext<WardrobeContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSqliteWasmDbContextFactory<WardrobeContext>(opt =>
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient();
+
 builder.Services.AddScoped<ImageService>();
 
 builder.RootComponents.Add<App>("#app");
@@ -22,8 +27,40 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var context = scope.ServiceProvider.GetRequiredService<WardrobeContext>();
-    context.Database.EnsureCreated();
+    var factory = scope.ServiceProvider.GetRequiredService<ISqliteWasmDbContextFactory<WardrobeContext>>();
+    var httpClientFactory = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>();
+
+    await using var context = await factory.CreateDbContextAsync(); 
+    var created = context.Database.EnsureCreated();
+    if (created)
+    {
+        await context.WardrobeItems.AddRangeAsync(
+            new WardrobeItem
+            {
+                Id = 1,
+                Name = "My favourite shirt",
+                Category = Category.Shirts,
+                Brand = "Grandma",
+                Size = Size.M,
+                ImageData = await ConvertImageToBytes("item1.png", httpClientFactory)
+            },
+            new WardrobeItem
+            {
+                Id = 2,
+                Name = "My favourite coat",
+                Category = Category.Dress,
+                Brand = "Prada",
+                Size = Size.L,
+                ImageData = await ConvertImageToBytes("item2.png", httpClientFactory)
+            }
+        );
+
+        async Task<byte[]> ConvertImageToBytes(string imageName, IHttpClientFactory factory)
+        {
+            var httpClient = factory.CreateClient();
+            return await httpClient.GetByteArrayAsync($"sample-images/{imageName}");
+        }
+    }
 }
 
 await app.RunAsync();
