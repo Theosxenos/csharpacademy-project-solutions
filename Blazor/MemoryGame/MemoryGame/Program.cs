bce8dc9e83e98aa2179f21ef50e20422e9118686
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-24 16:47:00 +0100

    Implement keeping and showing scores

--- Blazor/MemoryGame/MemoryGame/Program.cs
+++ Blazor/MemoryGame/MemoryGame/Program.cs
@@ -1,10 +1,13 @@
 using MemoryGame.Components;
+using MemoryGame.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<LocalStorageService>();
 
 var app = builder.Build();
 
