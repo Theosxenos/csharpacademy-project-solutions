ab8893d1b52831f2c2c0b65d4a0d1efadfef334a
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-25 16:16:17 +0100

    Create base for meal editor
    Create basic page/layout for the editor
    Add to menu
    Add seeding data to context for Food

--- Blazor/FoodJournal/FoodJournal/Data/FoodJournalContext.cs
+++ Blazor/FoodJournal/FoodJournal/Data/FoodJournalContext.cs
@@ -7,4 +7,45 @@ public class FoodJournalContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Meal> Meals { get; set; } = default!;
     public DbSet<Food> Foods { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Food>().HasData(
+            new Food
+            {
+                Id = 1,
+                Name = "Bread",
+                Icon = "icons8-kawaii-bread-96.png"
+            },
+            new Food
+            {
+                Id = 2,
+                Name = "Fruit",
+                Icon = "icons8-kawaii-grapes-96.png"
+            },
+            new Food
+            {
+                Id = 3,
+                Name = "Tea"
+            },
+            new Food
+            {
+                Id = 4,
+                Name = "Coffee",
+                Icon = "icons8-kawaii-coffee-96.png"
+            },
+            new Food
+            {
+                Id = 5,
+                Name = "Broccoli",
+                Icon = "icons8-kawaii-broccoli-96.png"
+            },
+            new Food
+            {
+                Id = 6,
+                Name = "Vegetable",
+                Icon = "icons8-cute-pumpkin-96.png"
+            }
+        );
+    }
 }
